52f028d6b163407b05e9540ad46d81820bd4192d
DanielHealy <danielhealyhosting3@gmail.com>
2023-01-03 21:32:04 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,93 @@
+const path = require("path");
+const { app, BrowserWindow, Menu } = require("electron");
+
+const isMac = process.platform === "darwin";
+const isDev = process.env.NODE_ENV !== "development";
+
+// create main window
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: isDev ? 1000 : 500,
+    height: 600,
+  });
+
+  // open dev tools if dev env
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+  // __dirname === cwd
+}
+
+// create about window
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
+// app ready
+app.whenReady().then(() => {
+  createWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Menu template
+// const menu = [{
+// 	label: 'file',
+// 	submenu: [{
+// 		label: 'quit',
+// 		click: () => app.quit(),
+// 		accelerator: 'CmdOrCtrl+W',
+// 	}]
+// }];
+
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  { role: "fileMenu" },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+];
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
